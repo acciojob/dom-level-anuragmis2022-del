5f9538dbbd360fc1540aa9727a9fbad1c5fadb58
anuragmis2022-del <anuragmis2022@gmail.com>
2026-06-06 20:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const level = document.getElementById("level")
+
+const current= level.parentElement;
+let count=1
+while(current)
+{
+	count++
+	current= current.parentElement
+}
+console.log("The level of the element is:"+count)
